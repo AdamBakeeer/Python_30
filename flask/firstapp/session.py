45159c02858abb26d3d1c243bd643959c3aa2212
adam bakeer <146668872+AdamBakeeer@users.noreply.github.com>
2024-09-12 00:03:30 +0000

    helloo

---
+++ flask/firstapp/session.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, session
+
+
+app = Flask(__name__, template_folder='templates')
+app.secret_key = 'SOME KEY'
+
+@app.route('/')
+def index3():
+    return render_template('index3.html')
+
+@app.route('/set_data')
+def set_data():
+    session['name'] = 'mike'
+    session['other'] = 'hello world'
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
